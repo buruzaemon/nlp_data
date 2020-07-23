78e94766f7fd13c1e0588a5a898f9068608d4837
buruzaemon <buruzaemon@io>
2020-07-23 12:13:55 +0900

    updated foo

--- tokenizer/foo.py
+++ tokenizer/foo.py
@@ -4,7 +4,10 @@ Created on Wed Jul 22 23:30:18 2020
 
 @author: buruzaemon
 """
+import os
 import spacy
+
+from os import path
 from spacy.lang.char_classes import ALPHA, \
                                     ALPHA_LOWER, \
                                     ALPHA_UPPER, \
@@ -14,36 +17,38 @@ from spacy.lang.char_classes import ALPHA, \
                                     LIST_ICONS
 from spacy.util import compile_infix_regex
 
-# default tokenizer
-nlp = spacy.load("en_core_web_sm")
 
-#txt = "mother-in-law"
-txt = "Foobar Inc. (8895.T)(BUY) is a winner. IOW, (8895 JP)(Buy)!"
+def main(new_model_names, orig_model_names):
 
-doc = nlp(txt)
-print([t.text for t in doc]) # ['mother', '-', 'in', '-', 'law']
+    # modify tokenizer infix patterns
+    infixes = (
+        LIST_ELLIPSES
+        + LIST_ICONS
+        + [
+            r"(?<=[0-9])[+\-\*^](?=[0-9-])",
+            r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(
+                al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
+            ),
+            r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+            r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
+            r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
+            # handle infix of )(
+            r"(?<=[{a}0-9])(\))".format(a=ALPHA),
+            r"(\()(?=[{a}0-9])".format(a=ALPHA),
+        ]
+    )
 
-# modify tokenizer infix patterns
-infixes = (
-    LIST_ELLIPSES
-    + LIST_ICONS
-    + [
-        r"(?<=[0-9])[+\-\*^](?=[0-9-])",
-        r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(
-            al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
-        ),
-        r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
-        r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
-        r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
-        # handle infix of )(
-        r"(?<=[{a}0-9])(\))".format(a=ALPHA),
-        r"(\()(?=[{a}0-9])".format(a=ALPHA),
-    ]
-)
+    infix_re = compile_infix_regex(infixes)
 
-infix_re = compile_infix_regex(infixes)
-nlp.tokenizer.infix_finditer = infix_re.finditer
+    for new_model,orig_model in zip(new_model_names, orig_model_names):
+        nlp = spacy.load(orig_model)
+        nlp.tokenizer.infix_finditer = infix_re.finditer
+
+        outdir = path.join(os.getcwd(), new_model)
+        nlp.to_disk(outdir)
 
 
-doc = nlp(txt)
-print([t.text for t in doc]) # ['mother-in-law']
+if __name__ == "__main__":
+    new_model_names = ['en_foobar_sm_base']
+    orig_model_names = ['en_core_web_sm']
+    main(new_model_names, orig_model_names)
